6d8147fdd0a91a7537cef0ea632fbde6a8afd9e8
Jefferson Carvalho <jeffersoncarvalhodosreis@gmail.com>
2025-11-01 19:31:28 -0300

    fix: retira botão de cadastrar procedimentos e especialidades para os usuários com permissão de recepção

--- src/pages/Specialists.vue
+++ src/pages/Specialists.vue
@@ -1,33 +1,35 @@
 <template>
   <specialist-table ref="specialistTableRef" :edit="true" :show-delete="true">
     <div class="flex gap-2">
-      <base-button-register @register="registerSpecialist = true" />
-
+      <base-button-register
+        v-if="['regulation_officer'].includes(role)"
+        @register="registerSpecialist = true"
+      />
     </div>
   </specialist-table>
-  <v-dialog
-    v-model="registerSpecialist"
-    class="z-999"
-  >
+  <v-dialog v-model="registerSpecialist" class="z-999">
     <specialist-form @close="registerSpecialist = false" @save="submit" />
   </v-dialog>
 </template>
 <script setup>
-  import { useSpecialistApi } from '@/composables/modules/useSpecialistModule';
-  import { useSweetAlertFeedback } from '@/composables/feedback/useSweetAlert';
+import { useSweetAlertFeedback } from "@/composables/feedback/useSweetAlert";
+import { useSpecialistApi } from "@/composables/modules/useSpecialistModule";
+import { useMeStore } from "@/stores/me";
 
-  const { create } = useSpecialistApi();
-  const { showFeedback } = useSweetAlertFeedback();
+const meStore = useMeStore();
+const role = computed(() => meStore.role);
 
-  const registerSpecialist = ref(false);
-  const specialistTableRef = ref(null);
+const { create } = useSpecialistApi();
+const { showFeedback } = useSweetAlertFeedback();
 
+const registerSpecialist = ref(false);
+const specialistTableRef = ref(null);
 
-  const submit = async specialist => {
-    const success = await showFeedback(() => create(specialist));
-    if (success) {
-      specialistTableRef.value?.refetch();
-      registerSpecialist.value = false;
-    }
+const submit = async (specialist) => {
+  const success = await showFeedback(() => create(specialist));
+  if (success) {
+    specialistTableRef.value?.refetch();
+    registerSpecialist.value = false;
   }
+};
 </script>
