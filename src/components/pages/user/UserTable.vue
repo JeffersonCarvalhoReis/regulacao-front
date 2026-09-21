7e92c91525e864d4d25b79e9e0e27f9aa343fba2
Jefferson Carvalho <jeffersoncarvalhodosreis@gmail.com>
2026-09-21 00:40:55 -0300

    feat: ajusta comportamento de rolagem em tabelas e melhora a estrutura do componente UserTable

--- src/components/pages/user/UserTable.vue
+++ src/components/pages/user/UserTable.vue
@@ -21,124 +21,115 @@
     <user-details :user-data="userData" @close="viewUserDetails = false" />
   </v-dialog>
 
-  <v-dialog v-model="editUser" class="z-999">
-    <user-form
-      :model-value="selectedUser"
-      @close="editUser = false"
-      @save="submit"
-    />
+  <v-dialog
+    v-model="editUser"
+    class="z-999"
+  >
+    <user-form :model-value="selectedUser" @close="editUser = false" @save="submit" />
   </v-dialog>
 </template>
 
 <script setup>
-import { useSweetAlertFeedback } from "@/composables/feedback/useSweetAlert";
-import { useUserApi } from "@/composables/modules/useUserModule";
-import debounce from "lodash/debounce";
+  import { useUserApi } from '@/composables/modules/useUserModule';
+  import { useSweetAlertFeedback } from '@/composables/feedback/useSweetAlert';
+  import debounce from 'lodash/debounce'
 
-const props = defineProps({
-  edit: { type: Boolean, default: false },
-  showDelete: { type: Boolean, default: false },
-});
-const {
-  data,
-  loadingList,
-  refetch,
-  setTableOptions,
-  meta,
-  setFilter,
-  update,
-  destroy,
-} = useUserApi();
-const { showFeedback, confirmModal } = useSweetAlertFeedback();
+  const props = defineProps({
+    edit: { type: Boolean, default: false },
+    showDelete: { type: Boolean, default: false },
 
-const options = ref({});
-const viewUserDetails = ref(false);
-const userData = ref({});
-const editUser = ref(false);
-const selectedUser = ref({});
+  })
+  const { data, loadingList, refetch, setTableOptions, meta, setFilter, update, destroy } = useUserApi();
+  const { showFeedback, confirmModal } = useSweetAlertFeedback();
 
-const updateOptions = (newOptions) => {
-  options.value = { ...newOptions };
-};
+  const options = ref({});
+  const viewUserDetails = ref(false);
+  const userData = ref({});
+  const editUser = ref(false);
+  const selectedUser = ref({});
 
-const handleEdit = (user) => {
-  selectedUser.value = user;
-  editUser.value = true;
-};
+  const updateOptions = newOptions => {
+    options.value = { ...newOptions }
+  };
 
-const submit = async (user) => {
-  await showFeedback(() => update(user.id, user));
-  refetch();
-  editUser.value = false;
-};
+  const handleEdit = user => {
+    selectedUser.value = user
+    editUser.value = true
+  };
 
-const handleDelete = async (user) => {
-  const confirm = await confirmModal(
-    `Tem certeza que deseja excluir o usuário <strong>${user.user}</strong>?`,
-    "Atenção",
-  );
-  if (confirm) {
-    await showFeedback(() => destroy(user));
+  const submit = async user => {
+    await showFeedback(() => update(user.id, user));
     refetch();
-  }
-};
+    editUser.value = false
+  };
 
-const search = debounce(async (v) => {
-  setFilter("user", v);
-  await nextTick();
-  refetch();
-}, 500);
+  const handleDelete = async user => {
+    const confirm = await confirmModal(`Tem certeza que deseja excluir o usuário <strong>${user.user}</strong>?`, 'Atenção');
+    if(confirm) {
+      await showFeedback(() => destroy(user)) ;
+      refetch()
+    }
+  };
 
-const viewUser = (v) => {
-  userData.value = v;
-  viewUserDetails.value = true;
-};
-
-watch(
-  () => options.value,
-  async (newOptions) => {
-    await nextTick();
-    setTableOptions(newOptions);
+  const search = debounce(async v => {
+    setFilter('user', v);
+    await nextTick()
     refetch();
-  },
-  { deep: true },
-);
+  }, 500);
 
-const headers = computed(() => {
-  const baseHeaders = [
-    {
-      title: "Detalhes",
-      value: "view",
-      align: "center",
-      width: "100px",
-      maxWidth: "100px",
-    },
-    {
-      title: "Usuário",
-      key: "user",
-      sortable: true,
-      align: "center",
-    },
-    {
-      title: "Unidade de Saúde",
-      key: "health_unit",
-      sortable: true,
-      align: "center",
-    },
-  ];
-  if (props.edit || props.showDelete) {
-    baseHeaders.push({
-      title: "Ações",
-      value: "action",
-      width: "200px",
-      maxWidth: "200px",
-      align: "center",
-    });
+  const viewUser = v => {
+    userData.value = v;
+    viewUserDetails.value = true;
   }
-  return baseHeaders;
-});
 
-defineExpose({
-  refetch,
-});
+
+  watch(
+    () => options.value,
+    async newOptions => {
+      await nextTick()
+      setTableOptions(newOptions)
+      refetch()
+    },
+    { deep: true }
+  )
+
+
+  const headers = computed( () => {
+    const baseHeaders = [
+      {
+        title: 'Detalhes',
+        value: 'view',
+        align: 'center',
+        width: '100px',
+      maxWidth: '100px',
+      },
+      {
+        title: 'Usuário',
+        key: 'user',
+        sortable: true,
+        align: 'center',
+      },
+      {
+        title: 'Unidade de Saúde',
+        key: 'health_unit',
+        sortable: true,
+        align: 'center',
+      },
+
+    ];
+    if(props.edit || props.showDelete) {
+      baseHeaders.push({
+        title: 'Ações',
+        value: 'action',
+        width: '200px',
+      maxWidth: '200px',
+        align: 'center',
+      });
+    }
+    return baseHeaders
+  });
+
+  defineExpose({
+    refetch,
+  });
 </script>
